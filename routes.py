8110a680ed9ef01ee41452eca2485ebd7a9523c1
Antti Kantola <antti.kantola@helsinki.fi>
2020-09-20 22:53:13 +0300

    @app.route /result added

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 #import visits
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def index():
@@ -8,3 +8,7 @@ def index():
 #    counter = visits.get_counter()
     return render_template("index.html")
     #, counter=counter)
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html",name=request.index["name"], weight=request.index["weight"])
